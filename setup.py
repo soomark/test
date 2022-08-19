a096867e69d9ef4d84d66b6f61275c6efcd33aaf
soomark <kamal.soomar@umuzi.org>
2022-08-19 11:09:31 +0200

    create repo

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sa_idnumber',
+    version='1.0',
+    packages=find_packages(),
+    license='none',
+    description='Validation program for SA id number',
+    install_requires=[],
+    url='NA',
+    author='soomark',
+    author_email='kamal.soomar@umuzi.org'
+)
